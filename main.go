8dfcd19f20d284c55e5bd9b43c1be993eb5035b5
Egor <khromov_dev@mail.ru>
2022-10-25 18:53:04 +0300

    hardcoded data and basic stuff

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type film struct {
+	ID       string  `json:"id"`
+	Title    string  `json:"title"`
+	Director string  `json:"director"`
+	Price    float32 `json:"price"`
+}
+
+var films = []film{
+	{ID: "1", Title: "The Hateful Eight", Director: "Quentin Tarantino", Price: 56.99},
+	{ID: "2", Title: "Eraserhead", Director: "David Lynch", Price: 17.99},
+	{ID: "3", Title: "No Country For Old Man", Director: "Coen brothers", Price: 39.99},
+}
+
+func getFilms(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, films)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/films", getFilms)
+
+}
